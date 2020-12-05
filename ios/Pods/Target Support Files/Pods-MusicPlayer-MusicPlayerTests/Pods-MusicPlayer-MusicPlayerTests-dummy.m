54beeb56920402bd1934e6e5ffaf7af386b41500
Trong Tran <trong.tn178@gmail.com>
2020-12-05 15:06:04 +0700

    Commit changes

--- ios/Pods/Target Support Files/Pods-MusicPlayer-MusicPlayerTests/Pods-MusicPlayer-MusicPlayerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MusicPlayer_MusicPlayerTests : NSObject
-@end
-@implementation PodsDummy_Pods_MusicPlayer_MusicPlayerTests
-@end
--- ios/Pods/Target Support Files/Pods-MusicPlayer-tvOS/Pods-MusicPlayer-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MusicPlayer_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_MusicPlayer_tvOS
-@end
